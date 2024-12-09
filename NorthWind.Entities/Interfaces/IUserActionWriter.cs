9f2d214e6b2ac259a51e2bf5f645bd2b56ecee2a
Alexandro Yule <alexandroyule@gmail.com>
2024-12-08 19:21:41 -0500

    Agregar archivos de proyecto.

---
+++ NorthWind.Entities/Interfaces/IUserActionWriter.cs
@@ -0,0 +1,12 @@
+﻿using NorthWind.Entities.ValueObjects;
+
+namespace NorthWind.Entities.Interfaces
+{
+    public interface IUserActionWriter
+    {
+        void Write(UserAction action)
+        {
+
+        }
+    }
+}
